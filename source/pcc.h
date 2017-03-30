2dc974d930d78cc50e2d5de043f33076792085fd
Andrew Montenigro <andrew.montenigro@gmail.com>
2017-03-30 14:35:44 -0400

    Add main header

---
+++ source/pcc.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include "keyword.h"
+#include "lexer.h"
+#include "status.h"
+
+RTN load_sources(int size, char** sources_arr, void* out){
+	return OK;
+}
+
