904577604d12e8adb1fda40ccc636723be652e13
Andrew Montenigro <andrew.montenigro@gmail.com>
2017-04-04 18:05:58 -0400

    Build arrays to store file pointers and contents

--- source/pcc.c
+++ source/pcc.c
@@ -11,7 +11,19 @@ int main(int argc, char** argv){
 	return 0;
 }
 
-enum RTN load_sources(int size, char** sources_arr, FILE* out){
+enum RTN load_sources(int size, char** sources_arr, FILE** out){
+	int* sizes;
+	int exists = 0;
+	sizes = malloc(sizeof(int) * size);
+	// Store filesizes in array, nonexistent files have size -1
+	for(int i = 0; i < size; i++){
+		sizes[i] = file_exists(sources_arr[i]);
+		if(!exists && (sizes[i] != -1)) exists = 1;
+	}
+	for(int i = 0; i < size; i++){
+		if(sizes[i] == -1)
+	}
+	free(sizes);
 	return OK;
 }
 enum RTN write_output(int size, unsigned char** output_arr, void* out){
--- source/pcc.h
+++ source/pcc.h
@@ -1,9 +1,16 @@
 #pragma once
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/stat.h>
 #include "keyword.h"
 #include "lexer.h"
 #include "status.h"
 
-enum RTN load_sources(int size, char** sources_arr, FILE* out);
+int file_size(const char* path){
+	struct stat st;
+	if(stat(filename, &st) == 0)
+		return st.size;
+	return -1;
+}
+enum RTN load_sources(int size, char** sources_arr, FILE** out);
 enum RTN write_output(int size, unsigned char** output_arr, void* out);
