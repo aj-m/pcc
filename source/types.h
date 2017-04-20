4560ea3589d34f809b11f3e5ef338b4fe53ffd9a
Andrew Montenigro <andrew.montenigro@gmail.com>
2017-04-20 18:15:08 -0400

    Add PB types header, stub PB constants header

---
+++ source/types.h
@@ -0,0 +1,19 @@
+#pragma once
+
+enum types {
+      t_PBPTR,
+      t_PBSHORT,
+      t_PBLONG,
+      t_PBQUAD,
+      t_PBBYTE,
+      t_PBWORD,
+      t_PBDWORD,
+      t_PBEXTENDED,
+      t_PBCURRENCY,
+      t_PBCURRENCYX,
+      t_PBSTRING,
+      t_PBSTRINGFL,
+      t_PBASCIIZ,
+      t_PBFIELD
+};
+
