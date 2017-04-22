c64a96738f892cb113139bb030ffddf6f84a7a7c
Andrew Montenigro <andrew.montenigro@gmail.com>
2017-04-22 13:33:53 -0400

    Fix struct stat member reference

--- source/pcc.h
+++ source/pcc.h
@@ -8,8 +8,8 @@
 
 int file_size(const char* path){
 	struct stat st;
-	if(stat(filename, &st) == 0)
-		return st.size;
+	if(stat(path, &st) == 0)
+		return st.st_size;
 	return -1;
 }
 enum RTN load_sources(int size, char** sources_arr, FILE** out);
